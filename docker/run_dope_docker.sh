488608d9688da64afa4ad4aa9ce4d8a6cc3b225e
Huck <febbo@umich.edu>
2020-01-14 10:51:14 -0800

    modified run script for docker container

--- docker/run_dope_docker.sh
+++ docker/run_dope_docker.sh
@@ -22,27 +22,33 @@ if [[ -z "${CONTAINER_DIR}" ]]; then
 fi
 
 STORAGE_CONTAINER=/data
-STORAGE_HOST=/data
+STORAGE_HOST=/home/hfebbo/data
 
 echo "Container name     : ${CONTAINER_NAME}"
 echo "Host directory     : ${HOST_DIR}"
 echo "Container directory: ${CONTAINER_DIR}"
 DOPE_ID=`docker ps -aqf "name=^/${CONTAINER_NAME}$"`
+
+docker rm nvidia-dope-v1
+echo "Creating new DOPE docker container."
+xhost +local:root
+nvidia-docker run --ipc=host -it --privileged --network=host -v ${HOST_DIR}:${CONTAINER_DIR}:rw -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v ${STORAGE_HOST}:${STORAGE_CONTAINER}:rw --env="DISPLAY" --name=${CONTAINER_NAME} nvidia-dope:kinetic-v1 bash
+
 #if [ 1 ]; then
-if [ -z "${DOPE_ID}" ]; then
-    echo "Creating new DOPE docker container."
-    xhost +local:root
-    nvidia-docker run -it --privileged --network=host -v ${HOST_DIR}:${CONTAINER_DIR}:rw -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v ${STORAGE_HOST}:${STORAGE_CONTAINER}:rw --env="DISPLAY" --name=${CONTAINER_NAME} nvidia-dope:kinetic-v1 bash
-else
-    echo "Found DOPE docker container: ${DOPE_ID}."
-    # Check if the container is already running and start if necessary.
-    if [ -z `docker ps -qf "name=^/${CONTAINER_NAME}$"` ]; then
-        xhost +local:${DOPE_ID}
-        echo "Starting and attaching to ${CONTAINER_NAME} container..."
-        docker start ${DOPE_ID}
-        docker attach ${DOPE_ID}
-    else
-        echo "Found running ${CONTAINER_NAME} container, attaching bash..."
-        docker exec -it ${DOPE_ID} bash
-    fi
-fi
+# if [ -z "${DOPE_ID}" ]; then
+    # echo "Creating new DOPE docker container."
+    # xhost +local:root
+    # nvidia-docker run --ipc=host -it --privileged --network=host -v ${HOST_DIR}:${CONTAINER_DIR}:rw -v /tmp/.X11-unix:/tmp/.X11-unix:rw -v ${STORAGE_HOST}:${STORAGE_CONTAINER}:rw --env="DISPLAY" --name=${CONTAINER_NAME} nvidia-dope:kinetic-v1 bash
+# else
+#     echo "Found DOPE docker container: ${DOPE_ID}."
+#     # Check if the container is already running and start if necessary.
+#     if [ -z `docker ps -qf "name=^/${CONTAINER_NAME}$"` ]; then
+#         xhost +local:${DOPE_ID}
+#         echo "Starting and attaching to ${CONTAINER_NAME} container..."
+#         docker start ${DOPE_ID}
+#         docker attach ${DOPE_ID}
+#     else
+#         echo "Found running ${CONTAINER_NAME} container, attaching bash..."
+#         docker exec -it ${DOPE_ID} bash
+#     fi
+# fi
